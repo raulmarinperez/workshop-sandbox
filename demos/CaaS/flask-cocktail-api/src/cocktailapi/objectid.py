20e9afbbde8f4949f0f003bdb63dec83417c62d4
Raul Marin <raulmarinperez@gmail.com>
2022-06-20 22:15:41 +0200

    Contents standardization

---
+++ demos/CaaS/flask-cocktail-api/src/cocktailapi/objectid.py
@@ -0,0 +1,45 @@
+# Copyright 2021 Roman Korolev
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+# This code was extracted and modified from the Beanie ODM at
+#
+#     https://github.com/roman-right/beanie
+
+
+from bson import ObjectId
+from pydantic.json import ENCODERS_BY_TYPE
+
+
+class PydanticObjectId(ObjectId):
+    """
+    Object Id field. Compatible with Pydantic.
+    """
+
+    @classmethod
+    def __get_validators__(cls):
+        yield cls.validate
+
+    @classmethod
+    def validate(cls, v):
+        return PydanticObjectId(v)
+
+    @classmethod
+    def __modify_schema__(cls, field_schema: dict):
+        field_schema.update(
+            type="string",
+            examples=["5eb7cf5a86d9755df3a6c593", "5eb7cfb05e32e07750a1756a"],
+        )
+
+
+ENCODERS_BY_TYPE[PydanticObjectId] = str
